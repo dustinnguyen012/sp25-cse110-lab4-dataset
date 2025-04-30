04122dc3a9604bebaba91b5afb41df1734c36573
dustinnguyen012 <dun012@ucsd.edu>
2025-04-30 04:31:54 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,14 +6,15 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof add !== 'boolean') {
-        return false;
-    }
-
     if (add) {
-        return num1 + num2;
-    } else {
-        return false;
+        const result = 0;
+
+        result = num1 + num2;
+
+        return result;
+    }
+    else {
+        return !add;
     }
 }
 
@@ -21,22 +22,18 @@ function sumValues(num1, num2, add) {
  * 
  * @param {*} prices, an array of the original price.
  * @param {*} discount, a number between 0-1 to represent the discount. 
- * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty or types are incorrect.
+ * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (!Array.isArray(prices) || typeof discount !== 'number') {
-        return false;
-    }
-
-    if (prices.length === 0) return false;
-
-    const discounted = [];
-    for (let i = 0; i < prices.length; i++) {
-        const discountedPrice = prices[i] * (1 - discount);
+    const discounted = []
+    const length = prices.length;
+    let discountedPrice = 0
+    for(let i = 0; i < length; i++) {
+        discountedPrice += prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
     return discounted;
 }
 
-module.exports = { sumValues, discountPrices };
+module.exports = {sumValues, discountPrices};
